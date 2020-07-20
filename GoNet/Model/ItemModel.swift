63aacf9bd5505a9c0fdf3325514604c72865020e
Carlos Ruiz <cruiz@dacompsc.com>
2020-07-20 16:59:43 -0500

    First commit

---
+++ GoNet/Model/ItemModel.swift
@@ -0,0 +1,15 @@
+//
+//  ItemModel.swift
+//  GoNet
+//
+//  Created by Carlos Ruiz on 20/07/20.
+//  Copyright © 2020 CERR. All rights reserved.
+//
+
+import UIKit
+
+struct ItemModel{
+    var title: String
+    var description: String
+    var imageURL: URL
+}
